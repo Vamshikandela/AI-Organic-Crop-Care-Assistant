9b44075d8d9151beb6966355aac1ea897932b925
VamshiKandela <154335754+Vamshikandela@users.noreply.github.com>
2026-09-04 18:58:05 +0530

    Update gemini_helper.py

--- utils/gemini_helper.py
+++ utils/gemini_helper.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
+model = genai.GenerativeModel("gemini-2.5-flash-lite")  ## sdk Software Developer Kit (SDK)
 
 
 def generate_farming_advice(prompt):
